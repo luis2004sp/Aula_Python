d087ac5e8c1c246b161607c15a57c6516481fa60
Luis <luisbruggermartine@gmail.com>
2023-05-25 16:18:20 -0300

    While com contador

--- Laço While.py
+++ Laço While.py
@@ -1,4 +1,4 @@
-numero_sorteado = 15
+"""numero_sorteado = 15
 
 numero_escolhido = int(input("Escolha um número entre 1 e 20: "))
 
@@ -6,4 +6,14 @@ while(numero_escolhido!=numero_sorteado):
     print("ERRROUUU! \nTente novamente")
     numero_escolhido = int(input("Escolha um número entre 1 e 20: "))
 
-print("ACERTOUUU!!!")
+print("ACERTOUUU!!!")"""
+
+
+#EX2: Estrutura com contador
+
+contador = 0
+
+while(contador<5):
+    print(contador)
+
+    contador = contador + 1
