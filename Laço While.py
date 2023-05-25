e076376affa7a31221003fdaff8d1c5696a539fc
Luis <luisbruggermartine@gmail.com>
2023-05-25 16:13:14 -0300

    Add jogo da sorte

---
+++ Laço While.py
@@ -0,0 +1,9 @@
+numero_sorteado = 15
+
+numero_escolhido = int(input("Escolha um número entre 1 e 20: "))
+
+while(numero_escolhido!=numero_sorteado):
+    print("ERRROUUU! \nTente novamente")
+    numero_escolhido = int(input("Escolha um número entre 1 e 20: "))
+
+print("ACERTOUUU!!!")
